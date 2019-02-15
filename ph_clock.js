f9e1f05f3bb36654f2d83ac49ad94a372783b0de
Unknown <jgordon024@west-mec.org>
2019-02-14 18:27:43 -0700

    Javascript
    created variables, added functions, and more. Gotta go...

---
+++ ph_clock.js
@@ -0,0 +1,58 @@
+"strict usage";
+/*
+   New Perspectives on HTML5 and CSS3, 7th Edition
+   Tutorial 9
+   Case Problem 4
+
+   Countdown Clock for Ticket Ordering
+   Author: Jacob Gordon
+   Date:  02/14/19
+
+   Filename:   ph_clock.js     
+
+*/
+
+var minsLeft = 0;
+var secsLeft = 15;
+var timeLeft = minsLeft * 60 + secsLeft;
+var clockID = setInterval("countdown()", 1000);
+
+
+
+
+function countdown() {
+    minsLeft = Math.floor(timeLeft / 60);
+    secsLeft = timeLeft - 60 * minsLeft;
+    var minsString = addLeadingZero(minsLeft);
+    var secsString = addLeadingZero(secsLeft);
+    document.getElementById("seconds").textContent = secsLeft;
+    document.getElementById("minutes").textContent = minsLeft
+    checkTimer();
+    timeLeft--;
+}
+
+function stopClock() {
+    document.getElementById("TimeHead").insertAdjacentHTML('beforeend',
+        " <br />(Order Expired)");
+    clearInterval(clockID);
+}
+
+
+/* ------------------------------------------------- */
+
+
+/* The checkTimer() function tests whether there is any time left to make the
+   ticket order. If the time left is 0, the stopClock() function is run;
+   otherwise nothing happens and the program continues to run. */
+
+function checkTimer() {
+    if (timeLeft === 0) stopClock();
+}
+
+
+/* The addLeadingZero() function adds a leading zero to values which are less than 10 */
+
+function addLeadingZero(num) {
+    var numStr = (num < 10) ? ("0" + num) : "" + num;
+    return numStr;
+}
